92723dd110761386a6e1e95e2adf1b78ea13e08f
Yacine <yacineheros2000@gmail.com>
2022-05-18 13:09:52 +0200

    Data Center 16/05/2022 M. Yacine BABI

--- microgrid/agents/data_center_agent.py
+++ microgrid/agents/data_center_agent.py
@@ -1,17 +1,62 @@
 import datetime
+import sys
+import os
+import pulp
+import pandas as pnd
 from microgrid.environments.data_center.data_center_env import DataCenterEnv
-
 
 class DataCenterAgent:
     def __init__(self, env: DataCenterEnv):
         self.env = env
 
-    def take_decision(self,
-                      state,
-                      previous_state=None,
-                      previous_action=None,
-                      previous_reward=None):
-        return self.env.action_space.sample()
+    def take_decision(self, state):
+        def data_center():
+            my_df = pnd.read_csv(r"C:\Users\yacin\Desktop\optim_et_energie\data_center\data_center_weekly_scenarios.csv",
+                                 sep=";")
+            my_df1 = my_df[my_df["scenario"] == 1]
+            L_IT = my_df1["cons (kW)"].values
+            dt = self.env.delta_t / datetime.timedelta(hours=1)
+            C1 = (self.env.COP_CS/ self.env.EER) * (1 / (dt * (self.env.COP_HP - 1)))
+            L_NF = (1 + 1 / (self.env.EER * dt)) * L_IT
+            Lambda = [2 for i in range(48)]
+            pwh = [0.5 for i in range(48)]
+            lp = pulp.LpProblem("DataCenter.lp", pulp.LpMinimize)
+            lp.setSolver()
+            alpha = [0 for i in range(48)]
+            for t in range(48):
+                # creation des variables
+                ###########################################################
+                var_name = "alpha" + str(t)
+                alpha[t] = pulp.LpVariable(var_name, 0.0, 1.0)
+
+                # creation des contraintes (a adapter)
+                ###########################################################
+                constraint_name = "Borne sup" + str(t)
+                lp += alpha[t] <= (self.env.max_transfert) / (dt * self.env.COP_HP * C1 * L_IT[t]), constraint_name
+                constraint_name="positive"+str(t)
+                lp+=alpha[t]>=0
+                constraint_name="inférieure à 1"+str(t)
+                lp+=alpha[t]<=1
+
+
+            # creation de la fonction objectif
+            ###########################################################
+            lp.setObjective(pulp.lpSum([Lambda[t] * (L_NF[t] + alpha[t] * C1*L_IT[t]) * dt - (self.env.COP_HP * dt * C1 * L_IT[t] * alpha[t] * pwh[t]) for t in range(48)]))
+            return lp
+        def run():
+            I=data_center()
+            I.solve()
+            R=[1 for i in range(48)]
+            for v in I.variables():
+                print(v.name,"=",v.varValue)
+                R[int(v.name[5:])] = v.varValue
+            return R
+        return run()
+
+
+
+
+
 
 
 if __name__ == "__main__":
